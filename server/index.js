290ccec8f334c2f5d026634e5f6380a5b7bf3501
formidabilus <chiriac_razvan@ymail.com>
2022-12-28 00:46:10 +0200

    added mongoose setup to index.js and setup .env file

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// CONFIGURATIONS
+//* CONFIGURATIONS
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-//  FILE STORAGE
+//*  FILE STORAGE
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/assets");
@@ -35,3 +35,16 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+//* MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+console.log(process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err.message} did not connect`));
