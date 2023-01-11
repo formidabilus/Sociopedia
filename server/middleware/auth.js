6f5384e0f713e3c585a393ab08c5ccf28c3bb2ac
formidabilus <chiriac_razvan@ymail.com>
2023-01-11 18:37:21 +0200

    finished and added UserWidget to HomePage and created a structure for the restof the widgets, fixed an error coming from auth.js middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
   try {
-    const token = req.header("Authorization");
+    let token = req.header("Authorization");
 
     if (!token) {
       return res.status(403).json({ msg: "No token, authorization denied" });
     }
 
     if (token.startsWith("Bearer ")) {
-      token = token.silce(7, token.length).trimLeft();
+      token = token.slice(7, token.length).trimLeft();
     }
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
