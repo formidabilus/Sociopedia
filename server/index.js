c71d8dc5803349faea54f95e664d71b063dbefbe
formidabilus <chiriac_razvan@ymail.com>
2023-02-06 01:10:46 +0200

    added app.get for '/*' in server.js

--- server/index.js
+++ server/index.js
@@ -25,6 +25,15 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
 app.get("/*", function (req, res) {
   res.sendFile(
     path.join(__dirname, "../client/public/index.html"),
@@ -36,15 +45,6 @@ app.get("/*", function (req, res) {
     }
   );
 });
-
-app.use(express());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //*  FILE STORAGE
 const storage = multer.diskStorage({
